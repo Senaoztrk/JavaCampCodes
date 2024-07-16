e8585cb75597137c904d0fbc4255db689ffa0a2d
Senanur Öztürk <sena20032015@hotmail.com>
2024-07-16 12:59:31 +0300

    ConditionBlocks

---
+++ ConditionBlocks/src/Main.java
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int not=45;
+        //50 ve üzeri geçti
+        //40-49 büt
+        //0-39 kaldı
+
+        if(not>=50)
+        {
+            System.out.println("Geçti");
+
+        }
+        else if(not>=40)
+        {
+            System.out.println("Bütünleme");
+
+        }
+        else
+        {
+            System.out.println("Kaldı");
+
+        }
+
+    }
+}
